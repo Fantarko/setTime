d15d2ff488fefba5a617c08f1051d5a16c2d7bb7
Fantarko <fantalopper@gmail.com>
2026-04-01 11:26:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public"))); 
 
 // --- ตัวแปรหลัก ---
-let time = 4800;      // เริ่มต้นที่ 1 ชั่วโมง (ปรับตามต้องการ)
+let time = 15000;      // เริ่มต้นที่ 1 ชั่วโมง (ปรับตามต้องการ)
 let isPaused = true;   // ตั้งค่าเริ่มต้นให้หยุดไว้ก่อน
 
 setInterval(() => {
